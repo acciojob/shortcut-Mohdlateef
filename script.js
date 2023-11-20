9bb3e6363d207d27ecb58f7e088ee5e1a63771d4
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-20 18:28:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function shortcut(s1, s2) {
   // your code here
-	let s3=s1.charAt(0);
-	let s4=s2.charAt(1);
-	
+		if(s1=""||s2="")
+	{
+		return "";
+	}
+	else{
+	let s3="";
+		s3=s1.charAt(0);
+	let s4=""
+		s4=s2.charAt(0);
+
+	}
 	return s3+s4;
+	
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(shortcut(s1, s2));
